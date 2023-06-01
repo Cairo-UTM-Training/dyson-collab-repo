10a3f16d5d542b320e1d2a020723f4304dda2cf3
aqillakhamis <aqillakhamis@gmail.com>
2023-06-01 11:09:33 +0800

    first commit

---
+++ operations.py
@@ -0,0 +1,24 @@
+"""
+This Program is used to do simple operations of add, subtract, multiply and divide. Please create function for the other operations (subtract, multiply and divide)
+"""
+
+# creating a function
+def add_numbers(a, b):
+    """
+    Adds two numbers and returns the result.
+    """
+    return a + b
+
+
+# Usage example:
+result = add_numbers(5, 3)
+print("Addition result:", result)
+
+result = subtract_numbers(10, 4)
+print("Subtraction result:", result)
+
+result = multiply_numbers(6, 2)
+print("Multiplication result:", result)
+
+result = divide_numbers(10, 2)
+print("Division result:", result)
